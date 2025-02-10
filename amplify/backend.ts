f0797c94fed267e848266ffb87e60ddcd696ba11
erastuskirui01 <erastuskirui01@gmail.com>
2025-02-10 05:00:16 +0000

    fixed auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,12 +2,14 @@
 import { defineBackend, secret } from '@aws-amplify/backend';
 // import { getSecret } from './functions/get-secret/resource';
 import { data } from './data/resource';
+import { auth } from './auth/resource';
 
 import { sayHello } from './functions/get-secret/resource';
 
-export const testApiKey = secret('testApiKey');
+// export const testApiKey = secret('testApiKey');
 
 defineBackend({
   sayHello,
+  auth,
   data
 });
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,12 +3,12 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 import { sayHello } from "../functions/get-secret/resource";
 
 const schema = a.schema({
-  // getSecret: a
-  //   .query()
-  //   .returns(a.string())
-  //   .handler(a.handler.function(getSecret)),
+  Todo: a
+    .model({
+      content: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 
-      
   sayHello: a
   .query()
   .arguments({
@@ -17,13 +17,15 @@ const schema = a.schema({
   .returns(a.string())
   .handler(a.handler.function(sayHello))
   .authorization(allow => [allow.publicApiKey()])
-  ,
 });
-
 export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "iam",
-  }
+    defaultAuthorizationMode: "apiKey",
+    // API Key is used for a.allow.public() rules
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
 });
